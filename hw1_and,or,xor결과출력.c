ec5456938da61e0cfc64d9420e3506f4b3dafd51
stellaJJ <121223615+stellaJJ@users.noreply.github.com>
2024-09-11 19:12:53 +0900

    Add files via upload

---
+++ hw1_and,or,xor결과출력.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void)
+{
+    int num1,num2;
+
+    printf("input two integers: \n");
+    scanf("%d %d",&num1,&num2);
+
+    int result1=num1&num2;
+    printf("%d&%d=%d \n",num1,num2,result1);
+
+    int result2=num1|num2;
+    printf("%d|%d=%d \n",num1,num2,result2);
+
+    int result3=num1^num2;
+    printf("%d^%d=%d",num1,num2,result3);
+    return 0;
+}
