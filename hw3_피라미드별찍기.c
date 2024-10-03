cb72ea4057037c846d8fcdd5a7d5ddfa9d20680c
stellaJJ <121223615+stellaJJ@users.noreply.github.com>
2024-10-03 19:46:37 +0900

    Update hw3_피라미드별찍기.c

--- hw3_피라미드별찍기.c
+++ hw3_피라미드별찍기.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-int main(void)
+int main (void)
 {
-   int num=5;
-        for(int i=0;i<num;i++)
-        {
-            for(int j=num-1;j>i;j--)
-            {
-                printf(" ");
-            }
-	    
-        for(int j=0;j<2*i+1;j++)
-        {
-	        printf("*");
-        }
+    int w, h, i; //w=가로, h 높이, i 공백
+
+    for (h=0; h<5; h++)
+    {
+        for (w=0; w<4-h; w++)
+    
+            printf(" ");
+
+        for (i=0; i<2*h+1; i++)
+            printf("*");
+
         printf("\n");
-        }
+    }
     return 0;
+
+
 }
+
